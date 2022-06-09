aa4ce3e10c2323aeb6407869b194404a4d89ecff
Gurbanov Tamerlan <t64566t@gmail.com>
2022-06-09 20:10:41 +0300

    fix: fixing test data not load and idea not see h2 db
    Fix issue when test data not load into h2 database. Moved .sql to another folder to prevent a bug. Switch h2 from mem to embedded to watch db in idea.

--- src/main/resources/data-test.sql
+++ src/main/resources/data-test.sql
@@ -1,13 +1,11 @@
-#Users
+
 insert into users (email, first_name, last_name, password, phone, username) values ('redstrike@gmail.com','Red','Strike','pass1','+380 63 108 7165','RedStriker');
 insert into users (email, first_name, last_name, password, phone, username) values ('darkstrike@gmail.com','Dark','Strike','pass2','+380 63 108 7165','DarkStriker');
 
-#Wallets
 insert into wallets (money_amount, money_currency, name, user_id) values (100.0,'USD','Defaut',1);
 insert into wallets (money_amount, money_currency, name, user_id) values (100.0,'USD','Defaut',2);
 insert into wallets (money_amount, money_currency, name, user_id) values (100.0,'USD','Special',1);
 
-#Roles
 insert into roles (name) values ('ROLE_UNVERIFIED');
 insert into roles (name) values ('ROLE_BLOCKED');
 insert into roles (name) values ('ROLE_CUSTOMER');
@@ -15,11 +13,9 @@ insert into roles (name) values ('ROLE_MASTER');
 insert into roles (name) values ('ROLE_MANAGER');
 insert into roles (name) values ('ROLE_ADMIN');
 
-#User_has_role
 insert into user_has_role (user_id, role_id) values (1,1);
 insert into user_has_role (user_id, role_id) values (1,3);
 insert into user_has_role (user_id, role_id) values (2,4);
 
-#User addresses
 insert into user_addresses (user_id, city, country, local_address) values (1,'Kyiv','Ukraine','some street');
 
