11022392ebc1853d70dc806d7cb37a21c1289286
Gurbanov Tamerlan <t64566t@gmail.com>
2022-06-10 14:58:30 +0300

    test: add test with save and delete
    Add test for check saving and delete of user.Add test cases findAll, addUserWithRoleAndFindByUsername, findByUsernameAndDelete

--- src/test/java/com/epam/finalproject/repository/UserRepositoryTest.java
+++ src/test/java/com/epam/finalproject/repository/UserRepositoryTest.java
@@ -1,26 +1,69 @@
 package com.epam.finalproject.repository;
 
-import com.epam.finalproject.RepairAgencyApplication;
+import com.epam.finalproject.entity.Role;
 import com.epam.finalproject.entity.RoleEnum;
 import com.epam.finalproject.entity.User;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest()
 @ActiveProfiles("test")
 class UserRepositoryTest {
 
     @Autowired
     UserRepository userRepository;
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    @Test
+    void findAll() {
+        List<User> users = userRepository.findAll();
+        assertFalse(users.isEmpty());
+    }
+
+    @Test
+    @Transactional
+    void findByUsernameAndDelete() {
+        User user = userRepository.findByUsername("RedStriker").orElseThrow();
+        userRepository.delete(user);
+        assertTrue(userRepository.findByUsername("RedStriker").isEmpty());
+    }
+
+    @Test
+    @Transactional
+    void addUserWithRoleAndFindByUsername() {
+        User user = User.builder()
+                .username("NoneStriker")
+                .email("nonestrike@gmail.com")
+                .password("pass3")
+                .firstName("None")
+                .lastName("Strike")
+                .phone("+380 63 108 7167")
+                .build();
+        Role role = roleRepository.findByName(RoleEnum.ROLE_UNVERIFIED).orElseThrow();
+        user.setRoles(Set.of(role));
+        user.setWallets(new HashSet<>());
+        userRepository.save(user);
+        User createdUser = userRepository.findByUsername("NoneStriker").orElseThrow();
+        assertNotNull(user.getWallets());
+        assertNotNull(user.getRoles());
+        assertTrue(user.getRoles().contains(role));
+        assertEquals("None", createdUser.getFirstName());
+        assertEquals("Strike", createdUser.getLastName());
+    }
 
     @Test
     void findByUsername() {
