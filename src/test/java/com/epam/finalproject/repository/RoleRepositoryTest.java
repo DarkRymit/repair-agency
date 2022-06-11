f9572e4c9c9a3ec5c517ea674f1632f408c6d8f2
Gurbanov Tamerlan <t64566t@gmail.com>
2022-06-11 17:14:27 +0300

    fix: remove unnecessary ExtendWith in test
    Removes unnecessary annotation @ExtendWith(SpringExtension.class) because it's already exists in @SpringBootTest().

--- src/test/java/com/epam/finalproject/repository/RoleRepositoryTest.java
+++ src/test/java/com/epam/finalproject/repository/RoleRepositoryTest.java
@@ -13,7 +13,6 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@ExtendWith(SpringExtension.class)
 @SpringBootTest()
 @ActiveProfiles("test")
 class RoleRepositoryTest {
--- src/test/java/com/epam/finalproject/repository/UserRepositoryTest.java
+++ src/test/java/com/epam/finalproject/repository/UserRepositoryTest.java
@@ -17,7 +17,6 @@ import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@ExtendWith(SpringExtension.class)
 @SpringBootTest()
 @ActiveProfiles("test")
 class UserRepositoryTest {
