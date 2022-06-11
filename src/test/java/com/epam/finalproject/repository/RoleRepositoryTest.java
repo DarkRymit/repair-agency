8c8dd5123b84ace9a403c819b4a3175aa652e560
Gurbanov Tamerlan <t64566t@gmail.com>
2022-06-11 16:59:45 +0300

    test: add tests to RoleRepository
    Adds test to RoleRepository with name findAll and findByName.

---
+++ src/test/java/com/epam/finalproject/repository/RoleRepositoryTest.java
@@ -0,0 +1,36 @@
+package com.epam.finalproject.repository;
+
+import com.epam.finalproject.entity.Role;
+import com.epam.finalproject.entity.RoleEnum;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest()
+@ActiveProfiles("test")
+class RoleRepositoryTest {
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    @Test
+    void findAll() {
+        List<Role> roles = roleRepository.findAll();
+        assertFalse(roles.isEmpty());
+    }
+
+    @Test
+    void findByName() {
+        Role role = roleRepository.findByName(RoleEnum.UNVERIFIED).orElseThrow();
+        assertEquals(role.getName(), RoleEnum.UNVERIFIED);
+    }
+
+}
