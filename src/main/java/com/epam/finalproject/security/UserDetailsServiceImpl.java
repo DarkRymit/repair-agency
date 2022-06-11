54e6eb3d33f95caad8eebbf76d7f85e3432b5df0
Gurbanov Tamerlan <t64566t@gmail.com>
2022-06-11 16:23:16 +0300

    feat(security): add custom UserDetailsService implementation.
    Add custom UserDetailsService implementation class named UserDetailsServiceImpl.

---
+++ src/main/java/com/epam/finalproject/security/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.epam.finalproject.security;
+
+
+import com.epam.finalproject.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private final UserRepository userRepository;
+
+    UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return UserDetailsImpl.of(userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Cant found " + username)));
+    }
+}
