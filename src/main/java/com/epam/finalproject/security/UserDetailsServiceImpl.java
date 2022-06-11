95869513741c1d4b8a6c1b637decaec148d989db
Gurbanov Tamerlan <t64566t@gmail.com>
2022-06-11 18:47:00 +0300

    fix(UserDetailsServiceImpl): remove unnecessary @Autowired
    Removes @Autowired from field userRepository in class UserDetailsServiceImpl.

--- src/main/java/com/epam/finalproject/security/UserDetailsServiceImpl.java
+++ src/main/java/com/epam/finalproject/security/UserDetailsServiceImpl.java
@@ -2,7 +2,6 @@ package com.epam.finalproject.security;
 
 
 import com.epam.finalproject.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -12,10 +11,9 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
-    @Autowired
     private final UserRepository userRepository;
 
-    UserDetailsServiceImpl(UserRepository userRepository) {
+    public UserDetailsServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
