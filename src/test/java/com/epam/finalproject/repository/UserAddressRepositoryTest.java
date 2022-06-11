09289cb4dc674f5f552ae25dfb6bb46dc38dc450
Gurbanov Tamerlan <t64566t@gmail.com>
2022-06-11 17:30:48 +0300

    test(repository): add test to UserAddressRepository
    Adds test to UserAddressRepository with name findAll and findByUser_Username.

---
+++ src/test/java/com/epam/finalproject/repository/UserAddressRepositoryTest.java
@@ -0,0 +1,33 @@
+package com.epam.finalproject.repository;
+
+import com.epam.finalproject.entity.UserAddress;
+import com.epam.finalproject.entity.Wallet;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest()
+@ActiveProfiles("test")
+class UserAddressRepositoryTest {
+
+    @Autowired
+    UserAddressRepository userAddressRepository;
+
+    @Test
+    void findAll() {
+        List<UserAddress> userAddresses = userAddressRepository.findAll();
+        assertFalse(userAddresses.isEmpty());
+    }
+
+    @Test
+    void findByUser_Username() {
+        UserAddress userAddress = userAddressRepository.findByUser_Username("RedStriker").orElseThrow();
+        assertEquals("Kyiv", userAddress.getCity());
+        assertEquals("Ukraine", userAddress.getCountry());
+    }
+}
