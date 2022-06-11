8bda47c075c10f4af17542bd55e2fd42b4a52f11
Gurbanov Tamerlan <t64566t@gmail.com>
2022-06-11 18:50:05 +0300

    test(security): add test to UserDetailsServiceImpl
    Adds test to UserDetailsServiceImpl with name of loadUserByUsername.This method using Mock try load UserDetailsImpl by username.

---
+++ src/test/java/com/epam/finalproject/security/UserDetailsServiceImplTest.java
@@ -0,0 +1,64 @@
+package com.epam.finalproject.security;
+
+import com.epam.finalproject.entity.Role;
+import com.epam.finalproject.entity.RoleEnum;
+import com.epam.finalproject.entity.User;
+import com.epam.finalproject.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Collection;
+import java.util.Optional;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+@ActiveProfiles("test")
+class UserDetailsServiceImplTest {
+
+    @Mock
+    UserRepository userRepository;
+
+    @InjectMocks
+    UserDetailsServiceImpl userDetailsService;
+
+    User user;
+
+    @BeforeEach
+    void setMockOutput() {
+        user = User.builder()
+                .username("MockStriker")
+                .email("mockstrike@gmail.com")
+                .firstName("Mock")
+                .password("secretPassword")
+                .lastName("Striker")
+                .phone("+380 63 108 7163")
+                .build();
+        user.setRoles(Set.of(new Role(RoleEnum.UNVERIFIED), new Role(RoleEnum.MASTER)));
+        when(userRepository.findByUsername("MockStriker")).thenReturn(Optional.of(user));
+    }
+
+    @Test
+    void loadUserByUsername() {
+        UserDetails userDetails = userDetailsService.loadUserByUsername("MockStriker");
+        assertNotNull(userDetails);
+        assertNotNull(userDetails.getUsername());
+        assertEquals(user.getUsername(), userDetails.getUsername());
+        assertNotNull(userDetails.getPassword());
+        assertEquals(user.getPassword(), userDetails.getPassword());
+        Collection<? extends GrantedAuthority> authority = userDetails.getAuthorities();
+        assertFalse(authority.isEmpty());
+        assertTrue(authority.contains(new SimpleGrantedAuthority("ROLE_" + RoleEnum.UNVERIFIED)));
+        assertTrue(authority.contains(new SimpleGrantedAuthority("ROLE_" + RoleEnum.MASTER)));
+    }
+}
