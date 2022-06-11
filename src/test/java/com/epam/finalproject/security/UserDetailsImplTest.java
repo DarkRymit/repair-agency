6a536203b8e16e1ecd397da5b2ed1cccc8911904
Gurbanov Tamerlan <t64566t@gmail.com>
2022-06-11 18:00:40 +0300

    test(security): add test to UserDetailsImpl
    Adds test to UserDetailsImpl with name of, getAuthorities, isAccountNonExpired, isAccountNonLocked, isCredentialsNonExpired, isEnabled, getUsername, getPassword.

---
+++ src/test/java/com/epam/finalproject/security/UserDetailsImplTest.java
@@ -0,0 +1,89 @@
+package com.epam.finalproject.security;
+
+import com.epam.finalproject.entity.Role;
+import com.epam.finalproject.entity.RoleEnum;
+import com.epam.finalproject.entity.User;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class UserDetailsImplTest {
+
+    static User user;
+
+    @BeforeAll
+    public static void setup() {
+        user = User.builder()
+                .username("NotDBStriker")
+                .email("notdbstrike@gmail.com")
+                .firstName("NotDB")
+                .password("secretPassword")
+                .lastName("Striker")
+                .phone("+380 63 108 7168")
+                .build();
+        user.setRoles(Set.of(new Role(RoleEnum.UNVERIFIED), new Role(RoleEnum.MANAGER)));
+    }
+
+    @Test
+    void of() {
+        UserDetails userDetails = UserDetailsImpl.of(user);
+        assertEquals(user.getUsername(), userDetails.getUsername());
+        assertEquals(user.getPassword(), userDetails.getPassword());
+        Collection<? extends GrantedAuthority> authority = userDetails.getAuthorities();
+        assertFalse(authority.isEmpty());
+
+    }
+
+    @Test
+    void getAuthorities() {
+        UserDetails userDetails = UserDetailsImpl.of(user);
+        Collection<? extends GrantedAuthority> authority = userDetails.getAuthorities();
+        assertFalse(authority.isEmpty());
+        assertTrue(authority.contains(new SimpleGrantedAuthority("ROLE_" + RoleEnum.UNVERIFIED)));
+        assertTrue(authority.contains(new SimpleGrantedAuthority("ROLE_" + RoleEnum.MANAGER)));
+    }
+
+    @Test
+    void isAccountNonExpired() {
+        UserDetails userDetails = UserDetailsImpl.of(user);
+        assertTrue(userDetails.isAccountNonExpired());
+    }
+
+    @Test
+    void isAccountNonLocked() {
+        UserDetails userDetails = UserDetailsImpl.of(user);
+        assertTrue(userDetails.isAccountNonLocked());
+    }
+
+    @Test
+    void isCredentialsNonExpired() {
+        UserDetails userDetails = UserDetailsImpl.of(user);
+        assertTrue(userDetails.isCredentialsNonExpired());
+    }
+
+    @Test
+    void isEnabled() {
+        UserDetails userDetails = UserDetailsImpl.of(user);
+        assertTrue(userDetails.isEnabled());
+    }
+
+    @Test
+    void getUsername() {
+        UserDetails userDetails = UserDetailsImpl.of(user);
+        assertEquals(user.getUsername(), userDetails.getUsername());
+    }
+
+    @Test
+    void getPassword() {
+        UserDetails userDetails = UserDetailsImpl.of(user);
+        assertEquals(user.getPassword(), userDetails.getPassword());
+    }
+}
