fe2e3cae7c532ad589fff3b2c0c13c60e0251993
Gurbanov Tamerlan <t64566t@gmail.com>
2022-06-11 16:25:02 +0300

    fix: change password in data-test
    Changes password to correct work with password length and BCryptPasswordEncoder.

--- src/main/resources/data-test.sql
+++ src/main/resources/data-test.sql
@@ -1,6 +1,6 @@
 
-insert into users (email, first_name, last_name, password, phone, username) values ('redstrike@gmail.com','Red','Strike','pass1','+380 63 108 7165','RedStriker');
-insert into users (email, first_name, last_name, password, phone, username) values ('darkstrike@gmail.com','Dark','Strike','pass2','+380 63 108 7165','DarkStriker');
+insert into users (email, first_name, last_name, password, phone, username) values ('redstrike@gmail.com','Red','Strike','$2a$10$jn4vz7Xh1u358Vi1K6.r5u7ipxbRP4Kj5sGw0Nl02NQ.IGRxRz/Ha','+380 63 108 7165','RedStriker');
+insert into users (email, first_name, last_name, password, phone, username) values ('darkstrike@gmail.com','Dark','Strike','$2a$10$JAYo8LApXbfu/93omHQ8Cu82AWzf7agoTkaLbazkqSpsFXwEGmGzm','+380 63 108 7165','DarkStriker');
 
 insert into wallets (money_amount, money_currency, name, user_id) values (100.0,'USD','Defaut',1);
 insert into wallets (money_amount, money_currency, name, user_id) values (100.0,'USD','Defaut',2);
