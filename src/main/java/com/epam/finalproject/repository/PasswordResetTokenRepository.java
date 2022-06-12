29357a54b9c600d515439ec0d2a6a5718e204df0
Gurbanov Tamerlan <t64566t@gmail.com>
2022-06-12 19:31:11 +0300

    feat(repositories): add PasswordResetTokenRepository and VerificationTokenRepository
    Adds repository PasswordResetTokenRepository and VerificationTokenRepository.

---
+++ src/main/java/com/epam/finalproject/repository/PasswordResetTokenRepository.java
@@ -0,0 +1,19 @@
+package com.epam.finalproject.repository;
+
+import com.epam.finalproject.entity.PasswordResetToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+
+@Repository
+public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
+
+    PasswordResetToken findByToken(String token);
+
+    @Modifying
+    @Query("delete from PasswordResetToken t where t.expiryDate <= ?1")
+    void deleteAllExpiredSince(Date now);
+}
---
+++ src/main/java/com/epam/finalproject/repository/VerificationTokenRepository.java
@@ -0,0 +1,21 @@
+package com.epam.finalproject.repository;
+
+
+import com.epam.finalproject.entity.VerificationToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+import java.util.Optional;
+
+@Repository
+public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Long> {
+
+    Optional<VerificationToken> findByToken(String token);
+
+    @Modifying
+    @Query("delete from VerificationToken t where t.expiryDate <= ?1")
+    void deleteAllExpiredSince(Date now);
+}
