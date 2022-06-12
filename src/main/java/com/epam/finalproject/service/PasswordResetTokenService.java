4798669d229a4555e55037cda3dfa70e2887f700
Gurbanov Tamerlan <t64566t@gmail.com>
2022-06-12 19:33:15 +0300

    feat(service): add interface PasswordResetTokenService and implementation
    Adds interface PasswordResetTokenService and ints default implementation named PasswordResetTokenServiceImpl.

---
+++ src/main/java/com/epam/finalproject/service/PasswordResetTokenService.java
@@ -0,0 +1,5 @@
+package com.epam.finalproject.service;
+
+public interface PasswordResetTokenService {
+
+}
---
+++ src/main/java/com/epam/finalproject/service/impl/PasswordResetTokenServiceImpl.java
@@ -0,0 +1,25 @@
+package com.epam.finalproject.service.impl;
+
+import com.epam.finalproject.service.PasswordResetTokenService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.Calendar;
+import java.util.Date;
+
+@Service
+public class PasswordResetTokenServiceImpl implements PasswordResetTokenService {
+
+    @Value("${token.reset.password.expiration}")
+    private Integer EXPIRATION;
+
+
+    private Date calculateExpiryDate(final int expiryTimeInMinutes) {
+        final Calendar cal = Calendar.getInstance();
+        cal.setTimeInMillis(new Date().getTime());
+        cal.add(Calendar.MINUTE, expiryTimeInMinutes);
+        return new Date(cal.getTime().getTime());
+    }
+
+}
