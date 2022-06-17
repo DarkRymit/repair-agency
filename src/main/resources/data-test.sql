fc350a8a288dd967938095317e240c3e4d6caaed
Gurbanov Tamerlan <t64566t@gmail.com>
2022-06-17 21:03:45 +0300

    feat(data-test.sql) add data to create Receipt entity
    Adds data to create Receipt entity. It includes data for tables receipts,receipt_items,receipt_deliveries,users,user_has_role.

--- src/main/resources/data-test.sql
+++ src/main/resources/data-test.sql
@@ -1,6 +1,8 @@
 
 insert into users (email, first_name, last_name, password, phone, username) values ('redstrike@gmail.com','Red','Strike','$2a$10$jn4vz7Xh1u358Vi1K6.r5u7ipxbRP4Kj5sGw0Nl02NQ.IGRxRz/Ha','+380 63 108 7165','RedStriker');
 insert into users (email, first_name, last_name, password, phone, username) values ('darkstrike@gmail.com','Dark','Strike','$2a$10$JAYo8LApXbfu/93omHQ8Cu82AWzf7agoTkaLbazkqSpsFXwEGmGzm','+380 63 108 7165','DarkStriker');
+insert into users (email, first_name, last_name, password, phone, username) values ('masterstrike@gmail.com','Master','Strike','$2a$10$JAYo8LApXbfu/93omHQ8Cu82AWzf7agoTkaLbazkqSpsFXwEGmGzm','+380 63 108 9165','MasterStriker');
+insert into users (email, first_name, last_name, password, phone, username) values ('customerstrike@gmail.com','Customer','Strike','$2a$10$jn4vz7Xh1u358Vi1K6.r5u7ipxbRP4Kj5sGw0Nl02NQ.IGRxRz/Ha','+380 63 108 9165','CustomerStriker');
 
 insert into wallets (money_amount, money_currency, name, user_id) values (100.0,'USD','Default',1);
 insert into wallets (money_amount, money_currency, name, user_id) values (100.0,'USD','Default',2);
@@ -16,6 +18,7 @@ insert into roles (name) values ('ADMIN');
 insert into user_has_role (user_id, role_id) values (1,1);
 insert into user_has_role (user_id, role_id) values (1,3);
 insert into user_has_role (user_id, role_id) values (2,4);
+insert into user_has_role (user_id, role_id) values (3,4);
 
 insert into user_addresses (user_id, city, country, local_address) values (1,'Kyiv','Ukraine','some street');
 
@@ -29,12 +32,20 @@ insert into repair_works (name, price_amount, price_currency,category_id) values
 insert into repair_works (name, price_amount, price_currency,category_id) values ('DATA_RECOVERY',10.50,'USD',2);
 insert into repair_works (name, price_amount, price_currency,category_id) values ('BATTERY_REPLACE',22.40,'USD',3);
 
-insert into REPAIR_WORK_HAS_STATUS (REPAIR_WORK_ID, STATUSES) values (1,'VIP');
-insert into REPAIR_WORK_HAS_STATUS (REPAIR_WORK_ID, STATUSES) values (2,'VIP');
-insert into REPAIR_WORK_HAS_STATUS (REPAIR_WORK_ID, STATUSES) values (2,'SUSPENDED');
+insert into repair_work_has_status (repair_work_id, statuses) values (1,'VIP');
+insert into repair_work_has_status (repair_work_id, statuses) values (2,'VIP');
+insert into repair_work_has_status (repair_work_id, statuses) values (2,'SUSPENDED');
 
 insert into receipt_statuses (name) values ('WAIT_FOR_PAYMENT');
 insert into receipt_statuses (name) values ('PAID');
 insert into receipt_statuses (name) values ('IN_WORK');
 insert into receipt_statuses (name) values ('DONE');
 insert into receipt_statuses (name) values ('CANCELED');
+
+insert into receipts (user_id,receipt_status_id,category_id,master_id,price_amount, price_currency,note,creation_time) values (4,2,1,3,84.9,'USD','Typical note','2022-01-10 14:23:22');
+
+insert into receipt_items (receipt_id,repair_work_id, price_amount, price_currency) values (1,1,20.40,'USD');
+insert into receipt_items (receipt_id,repair_work_id, price_amount, price_currency) values (1,2,52.40,'USD');
+insert into receipt_items (receipt_id,repair_work_id, price_amount, price_currency) values (1,3,12.10,'USD');
+
+insert into receipt_deliveries (receipt_id, city, country, local_address) values (1,'Kyiv','Ukraine','some street2');
