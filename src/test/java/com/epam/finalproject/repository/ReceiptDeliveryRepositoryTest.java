2b24e145a8f6ce1e78d741982bb349c44efd0614
Gurbanov Tamerlan <t64566t@gmail.com>
2022-06-17 21:41:59 +0300

    test(repository): add ReceiptDeliveryRepositoryTest
    Add ReceiptDeliveryRepositoryTest that include test methods find, findAll, findAllByReceipt_User_Username.

---
+++ src/test/java/com/epam/finalproject/repository/ReceiptDeliveryRepositoryTest.java
@@ -0,0 +1,43 @@
+package com.epam.finalproject.repository;
+
+import com.epam.finalproject.entity.Receipt;
+import com.epam.finalproject.entity.ReceiptDelivery;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest()
+@ActiveProfiles("test")
+class ReceiptDeliveryRepositoryTest {
+
+    @Autowired
+    ReceiptDeliveryRepository receiptDeliveryRepository;
+
+    @Test
+    void find() {
+        ReceiptDelivery receiptDeliveries = receiptDeliveryRepository.findById(1L).orElseThrow();
+        Receipt receipt = receiptDeliveries.getReceipt();
+        assertNotNull(receipt);
+        assertEquals(1L,receipt.getReceiptDelivery().getId());
+        assertEquals("Kyiv",receiptDeliveries.getCity());
+        assertEquals("Ukraine",receiptDeliveries.getCountry());
+        assertEquals("some street2",receiptDeliveries.getLocalAddress());
+    }
+
+    @Test
+    void findAll() {
+        List<ReceiptDelivery> receiptDeliveries = receiptDeliveryRepository.findAll();
+        assertFalse(receiptDeliveries.isEmpty());
+    }
+
+    @Test
+    void findAllByReceipt_User_Username() {
+        List<ReceiptDelivery> receiptDeliveries = receiptDeliveryRepository.findAllByReceipt_User_Username("CustomerStriker");
+        assertFalse(receiptDeliveries.isEmpty());
+    }
+}
