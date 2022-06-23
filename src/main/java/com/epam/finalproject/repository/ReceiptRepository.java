f59f18abd7b20ab0d5ae5c64e720cb363fdf477e
Gurbanov Tamerlan <t64566t@gmail.com>
2022-06-23 16:36:58 +0300

    feat(ReceiptRepository): extends interface with JpaSpecificationExecutor
    Extends ReceiptRepository interface with JpaSpecificationExecutor for use findAll(specification) etc.

--- src/main/java/com/epam/finalproject/repository/ReceiptRepository.java
+++ src/main/java/com/epam/finalproject/repository/ReceiptRepository.java
@@ -3,18 +3,18 @@ package com.epam.finalproject.repository;
 import com.epam.finalproject.entity.Receipt;
 import com.epam.finalproject.entity.ReceiptStatusEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
-public interface ReceiptRepository extends JpaRepository<Receipt, Long> {
+public interface ReceiptRepository extends JpaRepository<Receipt, Long>, JpaSpecificationExecutor<Receipt> {
     List<Receipt> findAllByUser_Username(String username);
     List<Receipt> findAllByMaster_Username(String username);
     List<Receipt> findAllByReceiptStatus_Name(ReceiptStatusEnum receiptStatusName);
     List<Receipt> findAllByCreationTimeBetween(LocalDateTime start, LocalDateTime end);
     List<Receipt> findAllByCreationTimeAfter(LocalDateTime time);
     List<Receipt> findAllByCreationTimeBefore(LocalDateTime time);
-
 }
