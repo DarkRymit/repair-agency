20db9991125ff6f12b9a54d8f892d21f8feb447a
Gurbanov Tamerlan <t64566t@gmail.com>
2022-06-23 16:40:46 +0300

    feat(project): add ReceiptSpecifications
    Adds ReceiptSpecifications with methods hasStatusWithName,hasMasterWithNameLike,hasUserWithNameLike and matchSearch. MatchSearch use receiptSearch and methods in ReceiptSpecifications to dynamic create Specification.

---
+++ src/main/java/com/epam/finalproject/repository/specification/ReceiptSpecifications.java
@@ -0,0 +1,55 @@
+package com.epam.finalproject.repository.specification;
+
+import com.epam.finalproject.entity.Receipt;
+import com.epam.finalproject.entity.ReceiptStatus;
+import com.epam.finalproject.entity.ReceiptStatusEnum;
+import com.epam.finalproject.entity.User;
+import com.epam.finalproject.search.ReceiptSearch;
+import org.springframework.data.jpa.domain.Specification;
+
+import javax.persistence.criteria.*;
+import java.util.Set;
+
+public class ReceiptSpecifications {
+
+    private ReceiptSpecifications() {
+    }
+
+    public static Specification<Receipt> hasStatusWithName(Set<ReceiptStatusEnum> status) {
+        return (root, query, cb) -> {
+            Join<ReceiptStatus, Receipt> receiptStatusReceiptJoin = root.join("receiptStatus");
+            return receiptStatusReceiptJoin.get("name").in(status);
+        };
+    }
+
+    public static Specification<Receipt> hasMasterWithNameLike(String name) {
+        return (root, query, cb) -> {
+            Join<User, Receipt> masterReceiptJoin = root.join("master");
+            return cb.like(masterReceiptJoin.get("username"), "%" + name + "%");
+        };
+    }
+    public static Specification<Receipt> hasUserWithNameLike(String name) {
+        return (root, query, cb) -> {
+            Join<User, Receipt> userReceiptJoin = root.join("user");
+            return cb.like(userReceiptJoin.get("username"), "%" + name + "%");
+        };
+    }
+
+    public static Specification<Receipt> matchSearch(ReceiptSearch receiptSearch) {
+        return (root, query, cb) -> {
+            Specification<Receipt> result = (root1, query1, criteriaBuilder) -> criteriaBuilder.and();
+
+            if (receiptSearch.getReceiptStatuses() != null && !receiptSearch.getReceiptStatuses().isEmpty()){
+                result = result.and(hasStatusWithName(receiptSearch.getReceiptStatuses()));
+            }
+            if(receiptSearch.getMasterUsername() != null &&!receiptSearch.getMasterUsername().isBlank()){
+                result = result.and(hasMasterWithNameLike(receiptSearch.getMasterUsername()));
+            }
+            if(receiptSearch.getUserUsername() != null &&!receiptSearch.getUserUsername().isBlank()){
+                result = result.and(hasUserWithNameLike(receiptSearch.getUserUsername()));
+            }
+            return result.toPredicate(root,query,cb);
+        };
+    }
+
+}
