4b8ecadf8d7c6094269338ad3cc0826c82c11e3e
Gurbanov Tamerlan <t64566t@gmail.com>
2022-07-01 15:26:30 +0300

    feat(Receipt): add builder
    Adds builder to Receipt class. To archive this also adds AllArgsConstructor and NoArgsConstructor.

--- src/main/java/com/epam/finalproject/model/entity/Receipt.java
+++ src/main/java/com/epam/finalproject/model/entity/Receipt.java
@@ -1,6 +1,9 @@
 package com.epam.finalproject.model.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.Fetch;
 import org.hibernate.annotations.FetchMode;
 import org.springframework.data.annotation.CreatedBy;
@@ -18,6 +21,9 @@ import java.util.Set;
 @Table(name = "receipts")
 @Data
 @EntityListeners(AuditingEntityListener.class)
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Receipt {
     @Id
     @Column(unique = true, nullable = false)
