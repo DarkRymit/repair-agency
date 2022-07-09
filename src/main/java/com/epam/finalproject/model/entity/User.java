69a7576ffdbbe656a946788e719eaab91c3fedc8
Gurbanov Tamerlan <t64566t@gmail.com>
2022-07-09 23:09:51 +0300

    refactor(User): delete Chainer, delete constructor, simplify Lombok
    Cleanups User class by deleting unused Chainer, constructor, simplify Lombok annotation changing to Lazy fetch.

--- src/main/java/com/epam/finalproject/model/entity/User.java
+++ src/main/java/com/epam/finalproject/model/entity/User.java
@@ -5,9 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.hibernate.annotations.Fetch;
-import org.hibernate.annotations.FetchMode;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -20,7 +17,6 @@ import java.util.Set;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder(toBuilder = true)
-@Slf4j
 public class User implements Serializable {
 
     @Id
@@ -44,38 +40,25 @@ public class User implements Serializable {
     @Column(nullable = false)
     private String phone;
 
-    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-    @Fetch(value = FetchMode.SUBSELECT)
+    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinTable(name = "user_has_role", joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"), inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+    @Builder.Default
     private Set<Role> roles = new HashSet<>();
 
-    @OneToMany(fetch = FetchType.EAGER, mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
-    @Fetch(value = FetchMode.SUBSELECT)
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Builder.Default
     private Set<Wallet> wallets = new HashSet<>();
 
     @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
     @PrimaryKeyJoinColumn()
     private UserAddress address;
 
-    public User(String username, String email, String password) {
-        this.username = username;
-        this.email = email;
-        this.password = password;
-        this.phone = email;
-    }
-
-    public Chainer chainer() {
-        return new Chainer(this);
-    }
-
     public void addRole(Role role) {
         this.roles.add(role);
-        role.getUsers().add(this);
     }
 
     public void deleteRole(Role role) {
         this.roles.remove(role);
-        role.getUsers().remove(this);
     }
 
     public void addWallet(Wallet wallet) {
@@ -86,70 +69,4 @@ public class User implements Serializable {
         this.wallets.remove(wallet);
     }
 
-    public static class Chainer {
-        User user;
-
-        private Chainer(User user) {
-            this.user = user;
-        }
-
-        public Chainer username(String username) {
-            user.setUsername(username);
-            return this;
-        }
-
-        public Chainer email(String email) {
-            user.setEmail(email);
-            return this;
-        }
-
-        public Chainer password(String password) {
-            user.setPassword(password);
-            return this;
-        }
-
-        public Chainer firstName(String firstName) {
-            user.setFirstName(firstName);
-            return this;
-        }
-
-        public Chainer lastName(String lastName) {
-            user.setLastName(lastName);
-            return this;
-        }
-
-        public Chainer phone(String phone) {
-            user.setPhone(phone);
-            return this;
-        }
-
-        public Chainer roles(Set<Role> roles) {
-            user.setRoles(roles);
-            return this;
-        }
-
-        public Chainer wallets(Set<Wallet> wallets) {
-            user.setWallets(wallets);
-            return this;
-        }
-
-        public Chainer address(UserAddress address) {
-            user.setAddress(address);
-            return this;
-        }
-
-        public Chainer addRole(Role role) {
-            user.addRole(role);
-            return this;
-        }
-
-        public Chainer addWallet(Wallet wallet) {
-            user.addWallet(wallet);
-            return this;
-        }
-
-        public User getTarget() {
-            return user;
-        }
-    }
 }
