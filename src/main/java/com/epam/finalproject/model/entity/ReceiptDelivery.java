c2d7d074f1344fc43c5b4de4d54af742ed2ceee5
Gurbanov Tamerlan <t64566t@gmail.com>
2022-07-09 22:18:38 +0300

    refactor(ReceiptDelivery): use Lombok Exclude to simplify infinite loop solve
    Changes solve of infinite loop using @EqualsAndHashCode.Exclude @ToString.Exclude.

--- src/main/java/com/epam/finalproject/model/entity/ReceiptDelivery.java
+++ src/main/java/com/epam/finalproject/model/entity/ReceiptDelivery.java
@@ -1,12 +1,8 @@
 package com.epam.finalproject.model.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
-import java.util.Objects;
 
 @Entity
 @Table(name = "receipt_deliveries")
@@ -21,6 +17,8 @@ public class ReceiptDelivery {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @EqualsAndHashCode.Exclude
+    @ToString.Exclude
     @OneToOne
     @MapsId
     @JoinColumn(nullable = false, name = "receipt_id")
@@ -38,29 +36,4 @@ public class ReceiptDelivery {
 
     private String postalCode;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        ReceiptDelivery that = (ReceiptDelivery) o;
-
-        if (!Objects.equals(id, that.id)) return false;
-        if (!Objects.equals(country, that.country)) return false;
-        if (!Objects.equals(state, that.state)) return false;
-        if (!Objects.equals(city, that.city)) return false;
-        if (!Objects.equals(localAddress, that.localAddress)) return false;
-        return Objects.equals(postalCode, that.postalCode);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id != null ? id.hashCode() : 0;
-        result = 31 * result + (country != null ? country.hashCode() : 0);
-        result = 31 * result + (state != null ? state.hashCode() : 0);
-        result = 31 * result + (city != null ? city.hashCode() : 0);
-        result = 31 * result + (localAddress != null ? localAddress.hashCode() : 0);
-        result = 31 * result + (postalCode != null ? postalCode.hashCode() : 0);
-        return result;
-    }
 }
