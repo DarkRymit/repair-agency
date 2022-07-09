6462bdd174d0e27a8618dc2f8013d245ee8dafc4
Gurbanov Tamerlan <t64566t@gmail.com>
2022-07-09 23:03:53 +0300

    feat(ReceiptItem): diff currency handle,add Lombok Exclude
    Adds possibility to handle diff currency to ReceiptItem by using global AppCurrency. Simplify code by Lombok @EqualsAndHashCode.Exclude, @ToString.Exclude.

--- src/main/java/com/epam/finalproject/model/entity/ReceiptItem.java
+++ src/main/java/com/epam/finalproject/model/entity/ReceiptItem.java
@@ -1,13 +1,9 @@
 package com.epam.finalproject.model.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
-import java.util.Objects;
 
 @Entity
 @Table(name = "receipt_items")
@@ -21,8 +17,9 @@ public class ReceiptItem {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @EqualsAndHashCode.Exclude
+    @ToString.Exclude
     @ManyToOne
-    @JoinColumn(nullable = false, name = "receipt_id")
     Receipt receipt;
 
     @ManyToOne
@@ -32,30 +29,8 @@ public class ReceiptItem {
     @Column(precision=19, scale=4)
     private BigDecimal priceAmount;
 
-    @Column
-    private String priceCurrency;
+    @ManyToOne
+    @JoinColumn(nullable = false,name = "currency_id")
+    private AppCurrency priceCurrency;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        ReceiptItem that = (ReceiptItem) o;
-
-        if (!Objects.equals(id, that.id)) return false;
-        if (receipt != null && receipt.getId() != null ? !receipt.getId().equals(that.receipt.getId()) : that.receipt != null) return false;
-        if (!Objects.equals(repairWork, that.repairWork)) return false;
-        if (!Objects.equals(priceAmount, that.priceAmount)) return false;
-        return Objects.equals(priceCurrency, that.priceCurrency);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id != null ? id.hashCode() : 0;
-        result = 31 * result + (receipt != null && receipt.getId() != null ? receipt.getId().hashCode(): 0);
-        result = 31 * result + (repairWork != null ? repairWork.hashCode() : 0);
-        result = 31 * result + (priceAmount != null ? priceAmount.hashCode() : 0);
-        result = 31 * result + (priceCurrency != null ? priceCurrency.hashCode() : 0);
-        return result;
-    }
 }
