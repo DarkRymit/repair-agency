6f254024aa7b731ccb1814e695e7836bd3951855
Gurbanov Tamerlan <t64566t@gmail.com>
2022-07-09 22:43:18 +0300

    refactor(UserAddress): use Lombok Exclude to simplify infinite loop solve
    Changes solve of infinite loop using @EqualsAndHashCode.Exclude @ToString.Exclude.

--- src/main/java/com/epam/finalproject/model/entity/UserAddress.java
+++ src/main/java/com/epam/finalproject/model/entity/UserAddress.java
@@ -2,11 +2,12 @@ package com.epam.finalproject.model.entity;
 
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import javax.persistence.*;
 import java.io.Serializable;
-import java.util.Objects;
 
 @Entity
 @Table(name = "user_addresses")
@@ -19,6 +20,8 @@ public class UserAddress implements Serializable {
     @Column(name = "user_id", unique = true)
     private Long id;
 
+    @EqualsAndHashCode.Exclude
+    @ToString.Exclude
     @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "user_id", referencedColumnName = "id", unique = true)
     @MapsId
@@ -36,44 +39,4 @@ public class UserAddress implements Serializable {
 
     private String postalCode;
 
-    @Override
-    public String toString() {
-        return "UserAddress{" +
-                "id=" + id +
-                ", user=" + (user != null ? user.getId() : null) +
-                ", country='" + country + '\'' +
-                ", state='" + state + '\'' +
-                ", city='" + city + '\'' +
-                ", localAddress='" + localAddress + '\'' +
-                ", postalCode='" + postalCode + '\'' +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        UserAddress that = (UserAddress) o;
-
-        if (!Objects.equals(id, that.id)) return false;
-        if (user != null ? !user.getId().equals(that.user.getId()) : that.user != null) return false;
-        if (!Objects.equals(country, that.country)) return false;
-        if (!Objects.equals(state, that.state)) return false;
-        if (!Objects.equals(city, that.city)) return false;
-        if (!Objects.equals(localAddress, that.localAddress)) return false;
-        return Objects.equals(postalCode, that.postalCode);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id != null ? id.hashCode() : 0;
-        result = 31 * result + (user != null ? user.getId().hashCode() : 0);
-        result = 31 * result + (country != null ? country.hashCode() : 0);
-        result = 31 * result + (state != null ? state.hashCode() : 0);
-        result = 31 * result + (city != null ? city.hashCode() : 0);
-        result = 31 * result + (localAddress != null ? localAddress.hashCode() : 0);
-        result = 31 * result + (postalCode != null ? postalCode.hashCode() : 0);
-        return result;
-    }
 }
