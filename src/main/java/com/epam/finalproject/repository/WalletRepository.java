a3312d4645b6340e87fc9b16d1f364084b4c36e7
Gurbanov Tamerlan <t64566t@gmail.com>
2022-07-10 00:32:28 +0300

    fix(WalletRepository) fix LazyInitializationException exception
    Fixes Hibernate LazyInitializationException exception using EntityGraph annotation.

--- src/main/java/com/epam/finalproject/repository/WalletRepository.java
+++ src/main/java/com/epam/finalproject/repository/WalletRepository.java
@@ -1,6 +1,7 @@
 package com.epam.finalproject.repository;
 
 import com.epam.finalproject.model.entity.Wallet;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,6 +10,8 @@ import java.util.Optional;
 
 @Repository
 public interface WalletRepository extends JpaRepository<Wallet, Long> {
+    @EntityGraph(attributePaths = {"user","moneyCurrency"})
     List<Wallet> findAllByUser_Username(String username);
+    @EntityGraph(attributePaths = {"user","moneyCurrency"})
     Optional<Wallet> findDistinctByNameAndUser_Username(String name, String username);
 }
