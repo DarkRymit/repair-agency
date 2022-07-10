09f29e76d8322ec02c37088cd91438c14f9267f1
Gurbanov Tamerlan <t64566t@gmail.com>
2022-07-10 17:33:52 +0300

    feat(ThymeleafUtil) format DataTime
    Adds format method to correct format ZonedDateTime to formatted string.

--- src/main/java/com/epam/finalproject/util/ThymeleafUtil.java
+++ src/main/java/com/epam/finalproject/util/ThymeleafUtil.java
@@ -3,16 +3,24 @@ package com.epam.finalproject.util;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+
 @Component
 public class ThymeleafUtil {
+
+    static DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM);
 
     private ThymeleafUtil() {
     }
 
     public static String replaceOrAddParam(String key, String value) {
-
         return ServletUriComponentsBuilder.fromCurrentRequest()
                 .replaceQueryParam(key, value)
                 .toUriString();
+    }
+    public static String format(ZonedDateTime zonedDateTime) {
+        return zonedDateTime.format(dateTimeFormatter);
     }
 }
