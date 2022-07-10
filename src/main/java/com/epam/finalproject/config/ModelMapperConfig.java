d382a763219380e68beaa1d151f3bb34bdd7e3dc
Gurbanov Tamerlan <t64566t@gmail.com>
2022-07-10 17:31:18 +0300

    feat(ModelMapper) map Instant to ZonedDateTime
    Adds to typeMap(Receipt.class, ReceiptDTO.class) mapping with instantZonedDateTimeConverter to convert Instant to ZonedDateTime.

--- src/main/java/com/epam/finalproject/config/ModelMapperConfig.java
+++ src/main/java/com/epam/finalproject/config/ModelMapperConfig.java
@@ -1,19 +1,33 @@
 package com.epam.finalproject.config;
 
+import com.epam.finalproject.dto.ReceiptDTO;
+import com.epam.finalproject.model.entity.Receipt;
 import com.epam.finalproject.model.entity.User;
 import com.epam.finalproject.payload.request.SignUpRequest;
 import lombok.AllArgsConstructor;
+import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
 @Configuration
 @AllArgsConstructor
 public class ModelMapperConfig {
+
+
     @Bean
     public ModelMapper modelMapper() {
         ModelMapper modelMapper = new ModelMapper();
         modelMapper.typeMap(SignUpRequest.class, User.class).addMappings(mapper -> mapper.skip(User::setPassword));
+        modelMapper.typeMap(Receipt.class, ReceiptDTO.class).addMappings(mapper -> mapper.using(instantZonedDateTimeConverter()).map(Receipt::getCreationDate,ReceiptDTO::setCreationDate));
         return modelMapper;
+    }
+    @Bean
+    public Converter<Instant, ZonedDateTime> instantZonedDateTimeConverter(){
+        return mappingContext -> mappingContext.getSource().atZone(ZoneId.systemDefault());
     }
 }
